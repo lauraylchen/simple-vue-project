73739d0119d231e01a9873d168bf8469c75f3f16
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-10-13 14:02:28 -0400

    tab functionality

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,54 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')">
+      Stored Resources
+    </base-button>
+    <base-button @click="setSelectedTab('add-resource')">
+      Add Resource
+    </base-button>
+  </base-card>
+  <keep-alive>
+    <component :is="selectedTab"></component>
+  </keep-alive>
+</template>
+
+<script>
+import AddResource from './AddResource.vue';
+import StoredResources from './StoredResources.vue';
+
+export default {
+  components: {
+    AddResource,
+    StoredResources,
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.org'
+        }
+      ]
+    };
+  },
+  provide() {
+    return {
+      resources: this.storedResources
+    }
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    }
+  }
+}
+</script>
