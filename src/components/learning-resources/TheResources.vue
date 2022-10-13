2b83c7b7f4fe760ae80a439353aaf1c324ed104d
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-10-13 14:25:31 -0400

    Added mode depending on selected tab

--- src/components/learning-resources/TheResources.vue
+++ src/components/learning-resources/TheResources.vue
@@ -1,9 +1,15 @@
 <template>
   <base-card>
-    <base-button @click="setSelectedTab('stored-resources')">
+    <base-button
+      @click="setSelectedTab('stored-resources')"
+      :mode="storedResButtonMode"
+    >
       Stored Resources
     </base-button>
-    <base-button @click="setSelectedTab('add-resource')">
+    <base-button
+      @click="setSelectedTab('add-resource')"
+      :mode="addResButtonMode"
+    >
       Add Resource
     </base-button>
   </base-card>
@@ -43,6 +49,14 @@ export default {
   provide() {
     return {
       resources: this.storedResources
+    }
+  },
+  computed: {
+    storedResButtonMode() {
+      return this.selectedTab === 'stored-resources' ? null : 'flat'
+    },
+    addResButtonMode() {
+      return this.selectedTab === 'add-resource' ? null : 'flat'
     }
   },
   methods: {
